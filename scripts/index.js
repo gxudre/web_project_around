65ae66b9653f8ea591f968f1d945fcc636d83947
Gustavo Xudre <gxudre@gmail.com>
2025-03-24 16:37:23 -0300

    alterações na pasta scripts

--- scripts/index.js
+++ scripts/index.js
@@ -17,3 +17,15 @@ likeBtns.forEach((likebtn) => {
     likeToggle(likebtn);
   });
 });
+
+const modal = document.querySelector(".modal");
+
+const openmodalbtn = document.querySelector(".profile__info-btn");
+const closemodalbtn = modal.querySelector(".modal__btn-sair");
+
+const toggleModal = () => {
+  modal.classList.toggle("modal__active");
+};
+
+openmodalbtn.addEventListener("click", toggleModal);
+closemodalbtn.addEventListener("click", toggleModal);
